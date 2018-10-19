1dfcab6a493c6257ccc4ee9985b815a13dde610c
Katie Dear <Katie@Katies-Air.home>
2018-10-19 07:19:37 +0100

    First commit

---
+++ PopUps/js/keyButton.js
@@ -0,0 +1,8 @@
+function openBox(e) {
+    var m = e.target;
+    
+    var borrow = document.getElementById("borrow");
+    borrow.innerHTML = m.borrow;
+    
+    var output = "<table><tr><td>Key Id</td>"
+}
---
+++ PopUps/js/open.js
@@ -0,0 +1,9 @@
+var modal = document.getElementById("box");
+
+var span = document.getElementById('close');
+
+var btn = document.getElementById('openBox');
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
